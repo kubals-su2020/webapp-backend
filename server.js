d7833409b27914939d57cd9cb4d51f66ebb1c5fd
kubal-s <kubal.s@husky.neu.edu>
2020-06-03 00:21:49 -0400

    changing mysql credentials

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use(bodyParser.urlencoded({
 var db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "rootpw",
-    database: "tdb_test"
+    password: "test@123",
+    database: "tdb_dev"
 });
 db.connect(function(err) {
     if (err) throw err;
