34fb2e21d1f766e03d706eb197f5cb1c72351dc1
kubal-s <kubal.s@husky.neu.edu>
2020-07-15 12:13:36 -0400

    removed echos and consoles

--- code_deploy/after_install.sh
+++ code_deploy/after_install.sh
@@ -5,5 +5,4 @@ sudo rm /opt/cloudwatch-config.json
 sudo cp /home/ubuntu/webapp_backend/cloud_watch/cloudwatch-config.json /opt/cloudwatch-config.json
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/cloudwatch-config.json -s
 sudo cp /home/ubuntu/webapp_backend/webapp-backend.service /lib/systemd/system/webapp-backend.service
-echo "enabling backend"
 sudo systemctl enable webapp-backend.service
